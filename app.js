b765b11913fdba6d136cac203170f3d58e2780cd
Your Name <you@example.com>
2022-06-06 22:18:18 -0500

    update template

--- app.js
+++ app.js
@@ -133,15 +133,10 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-        // const fs = require('fs');
-        // const generatePage = require('./src/page-template.js');
+        const pageHTML = generatePage(portfolioData);
+        fs.writeFile('./index.html', generatePage(name, github), err => {
+            if (err) throw new Error(err);
 
-        // const pageHTML = generatePage(name, github);
-
-        // fs.writeFile('./index.html', generatePage(name, github), err => {
-        //     if (err) throw new Error(err);
-
-        //     console.log('Portfolio complete! Check out index.html to see the output!');
-        // });
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });

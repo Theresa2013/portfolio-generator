59d0da0d9d37745fca5d71c1249a5dde3d4bbd3b
Your Name <you@example.com>
2022-06-07 20:09:44 -0500

    modify html template

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ promptUser()
     .then(promptProject)
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
-        fs.writeFile('./index.html', generatePage(name, github), err => {
+        fs.writeFile('./index.html', pageHTML, err => {
             if (err) throw new Error(err);
 
             console.log('Portfolio complete! Check out index.html to see the output!');
